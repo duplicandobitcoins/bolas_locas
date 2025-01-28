b7cd21a0c89a620f8e294b4b7dd86c38e33fa6ba
duplicandobitcoins <64798002+duplicandobitcoins@users.noreply.github.com>
2025-01-28 09:47:03 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,17 @@
 import os
 
+# Configuración de la base de datos
 DB_HOST = os.getenv("MYSQLHOST", "mysql.railway.internal")
-DB_PORT = os.getenv("MYSQLPORT", "3306")
+DB_PORT = int(os.getenv("MYSQLPORT", 3306))
 DB_USER = os.getenv("MYSQLUSER", "root")
 DB_PASSWORD = os.getenv("MYSQLPASSWORD", "tu_password_aqui")
 DB_NAME = os.getenv("MYSQLDATABASE", "railway")
+
+# Diccionario para conexiones
+db_config = {
+    "host": DB_HOST,
+    "port": DB_PORT,
+    "user": DB_USER,
+    "password": DB_PASSWORD,
+    "database": DB_NAME
+}
