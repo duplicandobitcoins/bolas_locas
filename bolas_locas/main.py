207e97f58f7b9d008f9281a644df73c35b013590
duplicandobitcoins <64798002+duplicandobitcoins@users.noreply.github.com>
2025-01-27 23:07:27 -0500

    Update main.py

--- bolas_locas/main.py
+++ bolas_locas/main.py
@@ -3,8 +3,11 @@ from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 import mysql.connector
 from config import DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME
+from bolas_locas.webhook import router as webhook_router
 
 app = FastAPI()
+
+app.include_router(webhook_router)
 
 # ✅ Función para conectar a la base de datos
 def get_db_connection():
